043ace016f1e4f94931537703385f886d54bc73c
LoesJanssen <loesjanssen89@gmail.com>
2020-01-18 11:44:40 +0100

    Add function total

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -10,6 +10,20 @@ class ShoppingCart {
     const item = { name: itemName, quantity: quantity, pricePerUnit: price };
     this.items.push(item);
   }
+  clear() {
+    return (this.items = []);
+  }
+  total() {
+    const totalShoppingCart = this.items.reduce(function(
+      totalValueSoFar,
+      item
+    ) {
+      const totalPricePerItem = item.quantity * item.pricePerUnit;
+      return totalValueSoFar + totalPricePerItem;
+    },
+    0);
+    return totalShoppingCart;
+  }
 }
 
 module.exports = ShoppingCart;
---
+++ src/testJavasScript.js
@@ -0,0 +1,19 @@
+const students = [
+  { name: "Berat", score: 100 },
+  { name: "Isabella", score: 84 },
+  { name: "Sophie", score: 99 },
+  { name: "Rein", score: 24 }
+];
+
+const totalValueShoppingCard = this.items.reduce(function(
+  totalValueSoFar,
+  number
+) {
+  console.log(
+    "TOTALE WAARDE SO FAR",
+    totalValueSoFar,
+    "CURRENT NUMBER",
+    number
+  );
+},
+0);
