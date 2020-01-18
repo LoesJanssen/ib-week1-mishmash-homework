c7f4a08332a18dcc6f7b906489d014d233b22bab
LoesJanssen <loesjanssen89@gmail.com>
2020-01-18 16:51:27 +0100

    Add adultsByRange

--- src/transform.js
+++ src/transform.js
@@ -1,22 +1,11 @@
 function groupAdultsByAgeRange(adults) {
-  // console.log("Adults:", adults); //geeft array met 2 objecten
-  // if (adults.length === 0) {
-  //   return {};
-  // }
-
-  const adultsAge18OrHigher = adults.reduce(function(totalAge, adult) {
+  const adultsByRange = adults.reduce(function(result, adult) {
     if (adult.age >= 18) {
-      return totalAge;
+      return result.push(adult);
     }
-    return adultsAge18OrHigher;
+    return result;
   }, {});
-
-  // const adultsAge18OrHigher = adults.filter(function(adult) {
-  //   // console.log(adult.age); geeft 10 & 17
-  //   return adult.age >= 18;
-  // });
-  // console.log(adultsAge18OrHigher);
-  // return adultsAge18OrHigher;
+  return adultsByRange;
 }
 
 module.exports.groupAdultsByAgeRange = groupAdultsByAgeRange;
