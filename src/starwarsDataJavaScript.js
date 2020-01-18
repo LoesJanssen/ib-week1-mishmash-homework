1ba56861a8b34085a9b72ccadafac3a2a6a7fd38
LoesJanssen <loesjanssen89@gmail.com>
2020-01-18 16:46:15 +0100

    Add starwarsData.html and starwarsDataJavaScript.js

--- src/starwarsDataJavaScript.js
+++ src/starwarsDataJavaScript.js
@@ -1,22 +1,27 @@
 const lukeSkywalkerInfoContainer = document.getElementById("lukeSkywalkerInfo");
-const darthVaderButton = document.getElementById("dartVaderButton");
+const darthVaderButton = document.getElementById("darthVaderButton");
+
+// blocked by CORS policy: No 'Access-Control-Allow-Origin'
 
 function getLukeSkywalkerInfo() {
   fetch("https://swapi.co/api/people/1")
     .then(response => response.json())
     .then(parsedResponse => {
-      lukeSkywalkerInfoContainer.innerHTML = parsedResponse.slip.advice;
+      console.log(parsedResponse);
+      lukeSkywalkerInfoContainer.innerHTML = parsedResponse.name;
     });
 }
+getLukeSkywalkerInfo();
+
+darthVaderButton.addEventListener("click", function() {
+  getDarthvaderInfo();
+});
 
 function getDarthvaderInfo() {
   fetch("https://swapi.co/api/people/4/ ")
     .then(response => response.json())
     .then(parsedResponse => {
-      darthVaderButton.innerHTML = parsedResponse.slip.advice;
+      console.log(parsedResponse);
+      lukeSkywalkerInfoContainer.innerHTML = parsedResponse.name;
     });
-
-  darthVaderButton.addEventListener("click", function() {
-    getDarthvaderInfo();
-  });
 }
